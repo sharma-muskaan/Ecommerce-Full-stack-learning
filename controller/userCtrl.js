6751f6a1682fb3b06de0cddbdffaf9ca39f6b68e
Muskaan Sharma <muskaan.sharma.us@gmail.com>
2023-05-25 16:01:28 -0400

    made newUser an await function

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -6,7 +6,7 @@ const createUser = async (req, res) => {
     const findUser = await User.findOne({ email:email });
     if (!findUser) {
         //* Create a new user
-        const newUser = User.create(req.body);
+        const newUser = await User.create(req.body);
         res.json(newUser); 
     }
 
