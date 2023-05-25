fa7d3ade7932cd804f46866fb118df0748090095
Muskaan Sharma <muskaan.sharma.us@gmail.com>
2023-05-25 14:56:39 -0400

    used a postman post request to see if inputted data will show up on mongo compass, it worked

---
+++ controller/userCtrl.js
@@ -0,0 +1,22 @@
+const User = require('../models/userModel');
+
+const createUser = async (req, res) => {
+    //* You cannot create a new user if they already exist. If you look at the user model then you can see the unique ID is for email, for this reason we are requesting the email of the user. 
+    const email = req.body.email;
+    const findUser = await User.findOne({ email:email });
+    if (!findUser) {
+        //* Create a new user
+        const newUser = User.create(req.body);
+        res.json(newUser); 
+    }
+
+    else {
+        //* User already exists
+        res.json({
+            msg: "User already exists",
+            success: false,
+        });
+    }
+};
+
+module.exports = { createUser };
