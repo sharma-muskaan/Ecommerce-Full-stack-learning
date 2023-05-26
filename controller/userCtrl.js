4b9f61d35be72b4e681c556c4bed51d3c55c734f
Muskaan Sharma <muskaan.sharma.us@gmail.com>
2023-05-26 09:57:06 -0400

    created login credentials, and password verification. also made 'user exists' functionality

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,5 +1,6 @@
 const User = require('../models/userModel');
 const asyncHandler = require("express-async-handler");
+const { generateToken } = require('../config/jwtToken');
 const createUser = asyncHandler(async (req, res) => {
     //* You cannot create a new user if they already exist. If you look at the user model then you can see the unique ID is for email, for this reason we are requesting the email of the user. 
     const email = req.body.email;
@@ -16,4 +17,26 @@ const createUser = asyncHandler(async (req, res) => {
     }
 });
 
-module.exports = { createUser };
+const loginUserCtrl = asyncHandler(async (req, res) => {
+    const {email, password} = req.body;
+    //* req.body is the schema we put in postman. 
+
+    //* Now we need to check if a particular user esists. 
+    const findUser = await User.findOne({ email });
+    if (findUser && await findUser.isPasswordMatched(password)) {
+        //* This is passing the password into userModel.js as enteredPassword.
+        res.json({
+            _id: findUser?._id,
+            firstname: findUser?.firstname,
+            lasname: findUser.lastname,
+            email: findUser?.email,
+            mobile: findUser?.mobile,
+            token: generateToken(findUser?.id),
+        });
+    }
+    else {
+        throw new Error("Invalid credentials");
+    }
+});
+
+module.exports = { createUser, loginUserCtrl };
